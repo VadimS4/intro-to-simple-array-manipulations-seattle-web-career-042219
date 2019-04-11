471a0c6990ad1c5dbd9b222f3661f0b73cd9e01e
Vadim Stakhnyuk <vstakhnyuk@outlook.com>
2019-04-11 08:51:07 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -32,6 +32,7 @@ def shift_with_args(array)
 end
 
 def using_concat(array, array)
-  
+  @my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
+  more_favs = ["mario kart", "flatiron school"]
 end
 
