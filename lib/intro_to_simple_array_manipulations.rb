864125010b26180431c5f9956ee40a8bd425b84c
Vadim Stakhnyuk <vstakhnyuk@outlook.com>
2019-04-11 08:18:50 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,6 +1,7 @@
 def using_push(array, string)
-  
+  updated_array = []
   countries_in_western_africa = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
   
+  updated_array = countries_in_western_africa.push("")
   
 end
