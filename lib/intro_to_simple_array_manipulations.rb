c2b3809d0ed590055e88b7a386ad441278800c3f
Vadim Stakhnyuk <vstakhnyuk@outlook.com>
2019-04-11 09:01:07 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -39,6 +39,7 @@ def using_concat(array1, array2)
 end
 
 def using_insert(array, element)
+  @list_of_esoteric_programming_languages = ["Ante", "ArnoldC", "Befunge", "Binary lambda calculus", "Chef", "GolfScript", "Ook!"]
   
 end
 
