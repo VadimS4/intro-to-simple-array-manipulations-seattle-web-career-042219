190026c9c6bd9ae277e54e239fee7594041ad67e
Vadim Stakhnyuk <vstakhnyuk@outlook.com>
2019-04-11 08:24:31 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -6,6 +6,7 @@ end
 
 def using_unshift(array, string)
   @updated_array = []
+  @new_neighborhood = ""
   neighborhoods_in_northwest_brooklyn = ["Brooklyn Yards", "Cadman Plaza", "Clinton Hill", "Downtown Brooklyn", "DUMBO"]
   @updated_array = @new_neighborhood.unshift
 end
