571bacb6debf4964c8d9d2d1c53284f8ec677732
Vadim Stakhnyuk <vstakhnyuk@outlook.com>
2019-04-11 08:33:47 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -8,9 +8,11 @@ def using_unshift(array, string)
   @new_neighborhood = "Brooklyn Heights"
   neighborhoods_in_northwest_brooklyn = ["Brooklyn Yards", "Cadman Plaza", "Clinton Hill", "Downtown Brooklyn", "DUMBO"]
   neighborhoods_in_northwest_brooklyn.unshift(@new_neighborhood)
+  return neighborhoods_in_northwest_brooklyn
 end
 
 def pop(array)
   @great_hits_of_the_nineties = ["Baby One More Time", "Smells Like Teen Spirit", "Missing", "Walking On The Sun", "Hard Knock Life", "Losing My Religion"]
-  
+  @great_hits_of_the_nineties.pop("Losing My Religion")
+  return @great_hits_of_the_nineties
 end
