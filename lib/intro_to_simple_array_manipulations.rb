14f6cb12ce9592cb16af191d40040ab69420fa2b
Vadim Stakhnyuk <vstakhnyuk@outlook.com>
2019-04-11 08:32:37 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -11,5 +11,6 @@ def using_unshift(array, string)
 end
 
 def pop(array)
+  @great_hits_of_the_nineties = ["Baby One More Time", "Smells Like Teen Spirit", "Missing", "Walking On The Sun", "Hard Knock Life", "Losing My Religion"]
   
 end
